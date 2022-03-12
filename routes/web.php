a22d8ab1d13779f43be57a589a12741086586753
sirjamesarua <dev.sirjames@gmail.com>
2022-03-11 20:30:24 -0800

    setup

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [App\Http\Controllers\PagesController::class, 'index']);
 Route::resource('posts','App\Http\Controllers\PostsController');
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/logout',function(){
