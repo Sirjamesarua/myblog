cb5a143c4e7b58465b8b7ca14c5a8a90241f906b
sirjamesarua <dev.sirjames@gmail.com>
2022-03-11 20:33:47 -0800

    setup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,7 @@
-<!--@extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
-<div class="container">
+<!--<div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -82,5 +82,6 @@
             </div>
         </div>
     </div>
-</div>
-@endsection-->
+</div>-->
+<center>YOU ARE NOT ALLOWED</center>
+@endsection
