2201c4bf1b4e9ab8e33abe83de03dfd848614335
sirjamesarua <dev.sirjames@gmail.com>
2022-03-11 19:46:08 -0800

    setup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if(env('APP_ENV') != 'local'){
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
     }
 }
